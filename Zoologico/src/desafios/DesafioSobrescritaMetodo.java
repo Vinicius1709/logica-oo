1d01516f11acb52f91203f7ec69f248586ebc52d
Vinícius Mendes Lopes <69206@PROWAY.TREINA>
2020-10-04 18:54:03 -0300

    Desafios

---
+++ Zoologico/src/desafios/DesafioSobrescritaMetodo.java
@@ -0,0 +1,48 @@
+package desafios;
+
+import java.util.Scanner;
+
+class AnimalDesafio{
+	
+	private String nome;
+	private String especie;
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getEspecie() {
+		return especie;
+	}
+	
+}
+
+class GatoDesafio extends AnimalDesafio{
+	
+	@Override
+	public String getEspecie() {
+		return getNome() + " é um gato";
+	}
+}
+
+public class DesafioSobrescritaMetodo {
+
+	public static void main(String[] args) {
+		
+		AnimalDesafio a = new GatoDesafio();
+		
+		Scanner scann = new Scanner(System.in);
+		a.setNome(scann.nextLine());
+		
+		System.out.println(a.getEspecie());
+		scann.close();
+	}
+}
